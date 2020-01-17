a54b41eef538132749a4da9beaf207eca4cbfa9b
Samir Omer <samiromer2@yahoo.com>
2020-01-17 11:54:25 +0200

    Add files via upload
    forms run down and adding the connection file

---
+++ lab2/page.php
@@ -0,0 +1,11 @@
+ <?php
+$servername = "localhost";
+$username = "root";
+$password = "oracle";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+
+?> 
